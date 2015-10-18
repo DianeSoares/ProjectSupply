d376bcaf5aa06780eff5ed72d12d66105f602eda
Diane <dianemichelli1@gmail.com>
2015-10-18 15:51:45 -0200

    Adding HighCharts

---
+++ src/main/webapp/resources/js/charts.js
@@ -0,0 +1,81 @@
+$(function () {
+	var chart = new Highcharts.Chart({
+        chart: {
+            renderTo: 'chart',
+            type: 'line'
+        },
+        title: {
+            text: 'Estoque'
+        },
+        subtitle: {
+            text: 'teste'
+        },
+        xAxis: {
+            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        },
+        yAxis: {
+            title: {
+                text: 'Quantidade'
+            }
+        },
+        plotOptions: {
+            line: {
+                dataLabels: {
+                    enabled: true
+                },
+                enableMouseTracking: false
+            }
+        },
+        series: [{
+            name: 'Estoque',
+            data: [1, 2, 3, 1, 5, 3, 7, 1, 9, 4, 8, 12]
+        }]
+    });
+});
+
+$(function () {
+	var chart1 = new Highcharts.Chart({
+        chart: {
+            renderTo: 'chartPie',
+            type: 'pie',
+            options3d: {
+                enabled: true,
+                alpha: 45,
+                beta: 0
+            }
+        },
+        title: {
+            text: 'Fornecedores 2015'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                depth: 35,
+                dataLabels: {
+                    enabled: true,
+                    format: '{point.name}'
+                }
+            }
+        },
+        series: [{
+            type: 'pie',
+            name: 'Fornecedores',
+            data: [
+                ['Vidros S.A',   45.0],
+                ['Metal S.A',       26.8],
+                {
+                    name: 'Peças S.A',
+                    y: 12.8,
+                    sliced: true,
+                    selected: true
+                },
+                ['Borracha S.A',    14.7],
+                ['Outros',   0.7]
+            ]
+        }]
+    });
+});
--- src/main/webapp/templates/header.jsp
+++ src/main/webapp/templates/header.jsp
@@ -38,10 +38,9 @@
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
-    
-     <script src="resources/js/validarCampos.js"></script>
+        <script src="resources/js/validarCampos.js"></script>
      
-     <script src="resources/js/jquery.js"></script>
+      <script src="resources/js/jquery.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
     <script src="resources/js/bootstrap.min.js"></script>
